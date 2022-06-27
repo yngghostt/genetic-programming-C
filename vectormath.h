ba6d92acbbb83bf81ffdd72641ae6101baba1991
ValeriaPichugina <70641956+ValeriaPichugina@users.noreply.github.com>
2022-06-27 23:21:06 +0300

    Update vectormath.h
    добавлены новые функции и исправлены ошибки в старых.

--- vectormath.h
+++ vectormath.h
@@ -1,13 +1,17 @@
 #ifndef NNET_VECTORMATH_H
 #define NNET_VECTORMATH_H
 
-double **dynamic_array_alloc( size_t N, size_t M );
-void dynamic_array_free( double **A, size_t N );
+extern double **dynamic_array_alloc( size_t N, size_t M );
+extern void dynamic_array_free( double **A, size_t N );
+extern void print( double *x, int n );
 extern double dot( double *v, double *y, int n );
 extern void mxv(  double **m,  double *v,double *res, int rows, int cols );
-void plus( double *x, double *y, double *res, int n );
-void minus( double *x, double *y, double *res, int n );
-void multiply( double *x, double a, double *res, int n );
-int max_ind(double *x, int n );
+extern void vxv( double *x, double *y, double **res, int n, int m );
+extern void plus( double *x, double *y, double *res, int n );
+extern void minus( double *x, double *y, double *res, int n );
+extern void multiply( double *x, double a, double *res, int n );
+extern double **transpose( double **m, int rows, int columns );
+extern int max_ind( double *x, int n );
+
 
 #endif

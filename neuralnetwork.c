af8ad68b0defd4f9f231532fbbb4a7a6d656a9b3
Danil Ovsyannikov <70640782+yngghostt@users.noreply.github.com>
2022-06-27 23:16:56 +0300

    Добавлено обратное распространение

--- neuralnetwork.c
+++ neuralnetwork.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "NEURALNETWORK.h"
 #include "vectormath.h"
@@ -25,44 +25,38 @@ double relu( double x )
 
 NEURALNETWORK init( void )
 {
-    double h1_biases[LAYER2];
-    double h2_biases[LAYER2];
-    double o_biases[LAYER3];
+    NEURALNETWORK network;
 
-    double **h1_weights = dynamic_array_alloc(LAYER2, LAYER1);
-    double **h2_weights = dynamic_array_alloc(LAYER2, LAYER2);
-    double **o_weights = dynamic_array_alloc(LAYER3, LAYER2);
+    network.h1_biases = (double *)malloc(LAYER2*sizeof(double));
+    network.h2_biases = (double *)malloc(LAYER2*sizeof(double));
+    network.o_biases = (double *)malloc(LAYER3*sizeof(double));
+
+    network.h1_weights = dynamic_array_alloc(LAYER2, LAYER1);
+    network.h2_weights = dynamic_array_alloc(LAYER2, LAYER2);
+    network.o_weights = dynamic_array_alloc(LAYER3, LAYER2);
 
     int i, j;
     for (i = 0; i < LAYER2; i++)
-        h1_biases[i] = (float) rand() / RAND_MAX * 6 - 3;
+        network.h1_biases[i] = (((double)rand())/RAND_MAX)*4 - 2;
+
     for (i = 0; i < LAYER2; i++)
-        h2_biases[i] = (float) rand() / RAND_MAX * 6 - 3;
-    for (i = 0; i < LAYER3; ++i)
-        o_biases[i] = (float) rand() / RAND_MAX * 6 - 3;
-    for (i = 0; i < LAYER2; ++i)
-        for (j = 0; j < LAYER1; ++j)
-            h1_weights[i][j] = (float) rand() / RAND_MAX * 6 - 3;
-    for (i = 0; i < LAYER2; ++i)
-        for (j = 0; j < LAYER2; ++j)
-            h2_weights[i][j] = (float) rand() / RAND_MAX * 6 - 3;
-    for (i = 0; i < LAYER3; ++i)
-        for (j = 0; j < LAYER2; ++j)
-            o_weights[i][j] = (float) rand() / RAND_MAX * 6 - 3;
+        network.h2_biases[i] = (((double)rand())/RAND_MAX)*4 - 2;
 
-    NEURALNETWORK network;
+    for (i = 0; i < LAYER3; i++)
+        network.o_biases[i] = (((double)rand())/RAND_MAX)*4 - 2;
 
-    network.h1_biases = h1_biases;
-    network.h2_biases = h2_biases;
-    network.o_biases = o_biases;
+    for (i = 0; i < LAYER2; i++)
+        for (j = 0; j < LAYER1; j++)
+            network.h1_weights[i][j] = (((double)rand())/RAND_MAX)*4 - 2;
 
-    network.h1_weights = h1_weights;
-    network.h2_weights = h2_biases;
-    network.o_weights = o_weights;
+    for (i = 0; i < LAYER2; i++)
+        for (j = 0; j < LAYER2; j++)
+            network.h2_weights[i][j] = (((double)rand())/RAND_MAX)*4 - 2;
 
-    dynamic_array_free(h1_weights, LAYER2);
-    dynamic_array_free(h2_weights, LAYER2);
-    dynamic_array_free(o_weights, LAYER3);
+    for (i = 0; i < LAYER3; i++)
+        for (j = 0; j < LAYER2; j++)
+            network.o_weights[i][j] = (((double)rand())/RAND_MAX)*4 - 2;
+
     return network;
 }
 
@@ -90,14 +84,82 @@ int forward_propagation( NEURALNETWORK *network, double *x ) {
     return max_ind(o, LAYER3);
 }
 
+void back_propagation( NEURALNETWORK *network, DELTA *delta, double *x, double *y )
+{
+    double z1[LAYER2];
+    double a1[LAYER2];
+
+    double z2[LAYER2];
+    double a2[LAYER2];
+
+    double z_o[LAYER3];
+    double predicted[LAYER3];
+
+    double delta1[LAYER3];
+    double delta2[LAYER2];
+    double delta3[LAYER2];
+
+    double tmp[LAYER3] = {0};
+
+    double **o_del_w = dynamic_array_alloc(LAYER3, LAYER2);
+    double **h2_del_w = dynamic_array_alloc(LAYER2, LAYER2);
+    double **h1_del_w = dynamic_array_alloc(LAYER2, LAYER1);
+
+    int i;
+
+    //Прямой проход
+    mxv(network->h1_weights, x, z1, LAYER2, LAYER1);
+    plus(z1, network->h1_biases, z1, LAYER2);
+    for (i = 0; i < LAYER2; i++)
+        a1[i] = sigmoid(z1[i]);
+
+    mxv(network->h2_weights, z1, z2, LAYER2, LAYER2);
+    plus(z2, network->h2_biases, z2, LAYER2);
+    for (i = 0; i < LAYER2; i++)
+        a2[i] = sigmoid(z2[i]);
+
+    mxv(network->o_weights, z1, z_o, LAYER3, LAYER2);
+    plus(z_o, network->o_biases, z_o, LAYER3);
+    for (i = 0; i < LAYER3; i++)
+        predicted[i] = sigmoid(z_o[i]);
+
+    //Обратный проход
+    minus(predicted, y, delta1, LAYER3);
+    for(i = 0; i < LAYER3; i++)
+    {
+        delta1[i] *= dsigmoid(z_o[i]);
+        tmp[i] = dsigmoid(z_o[i]);
+    }
+
+    vxv(delta1, a2, o_del_w,LAYER3, LAYER2);
+
+    double **o_weights_tr = transpose(network->o_weights, LAYER3, LAYER2);
+    mxv(o_weights_tr, delta1, delta2, LAYER2, LAYER3);
+    for(i = 0; i < LAYER2; i++)
+        delta2[i] *= dsigmoid(z2[i]);
+    vxv(delta2, a1, h2_del_w,LAYER2, LAYER2);
+
+    double **h2_weights_tr = transpose(network->h2_weights, LAYER2, LAYER2);
+    mxv(h2_weights_tr, delta2, delta3, LAYER2, LAYER2);
+    for(i = 0; i < LAYER2; i++)
+        delta3[i] *= dsigmoid(z1[i]);
+    vxv(delta1, x, h1_del_w,LAYER2, LAYER1);
+
+    dynamic_array_free(o_weights_tr, LAYER2);
+    dynamic_array_free(h2_weights_tr, LAYER2);
+
+    delta->o_del_b = delta1;
+    delta->h2_del_b = delta2;
+    delta->h1_del_b = delta3;
+
+    delta->o_del_w = o_del_w;
+    delta->h2_del_w = h2_del_w;
+    delta->h1_del_w = h1_del_w;
+}
+
 void update_batch( NEURALNETWORK *network, double *batch, int l_rate )
 {
 
-}
-
-double backprop( NEURALNETWORK *network, double *x, double *y )
-{
-    return 0;
 }
 
 void fit( NEURALNETWORK *network, double data, int epochs, int mini_batch_size, int l_rate )

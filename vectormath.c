eb1f38297ae1dc549dd9321f983d937c02f98a2f
ValeriaPichugina <70641956+ValeriaPichugina@users.noreply.github.com>
2022-06-27 23:23:53 +0300

    Update vectormath.c
    Добавление функций.

--- vectormath.c
+++ vectormath.c
@@ -1,4 +1,5 @@
 #include <limits.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 double **dynamic_array_alloc(size_t N, size_t M)
@@ -20,6 +21,16 @@ void dynamic_array_free(double **A, size_t N)
     free(A);
 }
 
+void print( double *x, int n )
+{
+    int i;
+    for(i = 0; i < n; i++)
+    {
+        printf("%.10f ", x[i]);
+    }
+    printf("\n-----\n");
+}
+
 double dot( double *x, double *y, int n)
 {
     double res = 0;
@@ -39,6 +50,14 @@ void mxv( double **m,  double *v, double *res, int rows, int cols )
     {
         res[i] = dot(m[i], v, cols);
     }
+}
+
+void vxv( double *x, double *y, double **res, int n, int m )
+{
+    int i, j;
+    for (i = 0; i < n; i++)
+        for(j = 0; j < m; j++)
+            res[i][j] = x[i]*y[j];
 }
 
 void plus( double *x, double *y, double *res, int n )
@@ -67,14 +86,25 @@ void multiply( double *x, double a, double *res, int n )
         res[i] = x[i]*a;
     }
 }
+double **transpose( double **m, int rows, int columns )
+{
+    double **res = dynamic_array_alloc(columns, rows);
+    int i, j;
+    for(i = 0; i < rows; i++)
+        for(j = 0; j < columns; j++)
+            res[j][i] = m[i][j];
+
+    return res;
+}
 
 int max_ind( double *x, int n )
 {
     int i, j = 0;
-    double min = LONG_MAX;
+    double max = 0;
     for(i = 0; i < n; i++) {
-        if (x[i] < min)
+        if (x[i] > max)
             j = i;
+        max = x[i];
     }
     return j;
 }

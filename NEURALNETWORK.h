a3d1b89bba74d16c62b2ec36d6c5a2b18d85a4c3
qxygen <52496357+qxygen@users.noreply.github.com>
2022-06-27 03:08:05 +0300

    Add files via upload

--- NEURALNETWORK.h
+++ NEURALNETWORK.h
@@ -1,15 +1,25 @@
-#ifndef NNET_NEURALNETWORK_H
-#define NNET_NEURALNETWORK_H
+#ifndef NNET_TEST_NEURALNETWORK_H
+#define NNET_TEST_NEURALNETWORK_H
+
+#define LAYER1 784
+#define LAYER2 100
+#define LAYER3 10
+
 
 typedef struct {
+    double *h1_biases;
+    double *h2_biases;
+    double *o_biases;
 
-    double h1_biases[100];
-    double h2_biases[100];
-    double o_biases[10];
-
-    double h1_weights[100][784];
-    double h2_weights[100][100];
-    double o_weights[10][100];
+    double **h1_weights;
+    double **h2_weights;
+    double **o_weights;
 } NEURALNETWORK;
+
+NEURALNETWORK init( void );
+int forward_propagation( NEURALNETWORK *network, double *x );
+void update_batch( NEURALNETWORK *network, double *batch, int l_rate );
+double backprop( NEURALNETWORK *network, double *x, double *y );
+void fit( NEURALNETWORK *network, double data, int epochs, int mini_batch_size, int l_rate );
 
 #endif
